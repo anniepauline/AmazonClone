12568891eae93ece04a9138fcb8c281b69c1611b
anniepauline <anniepauline.23@gmail.com>
2023-10-12 10:01:18 +0530

    firebase deploy

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA6mV7p-iXtwjY89G8EMey_9Vy68Z8J_b4",
+    authDomain: "challenge-6bd22.firebaseapp.com",
+    projectId: "challenge-6bd22",
+    storageBucket: "challenge-6bd22.appspot.com",
+    messagingSenderId: "528702478551",
+    appId: "1:528702478551:web:6683888f39ef084ef0b892"
+};
